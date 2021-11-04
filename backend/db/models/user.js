f0f8a5f372eda995b09a670a4b9e7922eb5517bf
Rob Strasser <roestrasser@sbcglobal.net>
2021-11-04 13:00:19 -0700

    user auth in model file

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -45,6 +46,51 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
   });
+
+  // returns User instance with only User instance info that is safe to save to a JWT
+  User.prototype.toSafeObject = function() {
+    const { id, username, email } = this;
+    return { id, username, email };
+  };
+
+  // checks if the password passed in matches password stored to User instance
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString())
+  };
+
+  // uses currentUser scope to return the user with that id
+  User.getCurrentUserById = async function(id) {
+    return await User.scope('currentUser').findByPk(id);
+  }
+
+  // accepts object and checks credentials for username OR email
+  // if user exists and PW matches, login
+  User.login = async function ({ credential, password }) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+
+  // hashes password and logs in user after signup
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  }
+
   User.associate = function(models) {
     // associations can be defined here
   };
