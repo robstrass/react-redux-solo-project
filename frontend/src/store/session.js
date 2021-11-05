c9476765175d5be0ac9422b89434738fbb7c2048
Rob Strasser <roestrasser@sbcglobal.net>
2021-11-05 15:28:13 -0700

    restore user functionality

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -29,9 +29,16 @@ export const login = (user) => async (dispatch) => {
     return response;
 }
 
-const initialState = { user: null };
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
 
 // reducer
+const initialState = { user: null };
+
 const sessionReducer = (state = initialState, action) => {
     let newState = {};
     switch (action.type) {
