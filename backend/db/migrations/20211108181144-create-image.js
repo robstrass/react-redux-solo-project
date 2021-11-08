a33d1ed927a7355816aab534074669ea521646c2
Rob Strasser <roestrasser@sbcglobal.net>
2021-11-08 11:10:51 -0800

    added models for database

---
+++ backend/db/migrations/20211108181144-create-image.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: { model: 'Users' },
+        type: Sequelize.INTEGER
+      },
+      albumId: {
+        allowNull: false,
+        references: { model: 'Albums' },
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Images');
+  }
+};
---
+++ backend/db/migrations/20211108181554-create-comment.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: { model: 'Users' },
+        type: Sequelize.INTEGER
+      },
+      imageId: {
+        allowNull: false,
+        references: { model: 'Images' },
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
