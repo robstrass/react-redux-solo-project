7a62f48696aa3e532c536ab624d15077a0b8a11b
Rob Strasser <roestrasser@sbcglobal.net>
2021-11-10 13:34:55 -0800

    backend route for posting images added and tested

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check, validationResult } = require('express-validator');
 const { restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
 
-const { User, Image, Album } = require('../../db/models')
+const { Image } = require('../../db/models')
 
 const router = express.Router();
+
+const imageValidation = [
+    check('imageUrl')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an image URL.'),
+    check('content')
+        .exists({ checkFalsy: true })
+        .withMessage('Please give a description of your photo.')
+]
 
 // All of a User's Images
 router.get('/:userId(\\d+)/images', restoreUser, asyncHandler(async (req, res) => {
@@ -16,6 +27,26 @@ router.get('/:userId(\\d+)/images', restoreUser, asyncHandler(async (req, res) =
         }
     })
     res.json(images);
+}));
+
+router.post('/:id(\\d+)/images', imageValidation, asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { userId, albumId, imageUrl, content } = req.body;
+    const imageErrors = validationResult(req);
+
+    if (+id === +userId && imageErrors.isEmpty()) {
+        const newImg = await Image.build({
+            userId,
+            albumId,
+            imageUrl,
+            content
+        });
+        await newImg.save();
+        res.json(newImg);
+    } else {
+        let errors = imageErrors.array().map((error) => error.msg);
+        res.json({ errors });
+    }
 }));
 
 module.exports = router;
