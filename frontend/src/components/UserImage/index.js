719c64dfb565e104741858b50497f87e3ff0ad82
Rob Strasser <roestrasser@sbcglobal.net>
2021-11-10 18:23:28 -0800

    modal context created, renders when clicking on delete

--- frontend/src/components/UserImage/index.js
+++ frontend/src/components/UserImage/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Redirect } from 'react-router-dom';
 
 import { loadOneImage } from '../../store/userImages';
+import { Modal } from '../../context/Model';
 
 import './UserImage.css';
 
@@ -10,6 +11,8 @@ function UserImage() {
     const dispatch = useDispatch();
     const { id } = useParams();
     const image = useSelector((state) => (state.userImage.current));
+
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => (
         dispatch(loadOneImage(id))
@@ -31,7 +34,12 @@ function UserImage() {
                 <div className = 'profile-single-img-content'>{image.content}</div>
                 <div className = 'profile-single-img-buttons'>
                     <button>Edit</button>
-                    <button>Delete</button>
+                    <button onClick = {() => setShowModal(true)}>Delete</button>
+                    { showModal && (
+                        <Modal onClose = {() => setShowModal(false)}>
+                            <h2>fuck you</h2>
+                        </Modal>
+                    )}
                 </div>
             </div>
         </div>
