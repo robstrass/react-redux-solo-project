1fa95c91654112c3450e7230fc117f1c24e1f8ef
Rob Strasser <roestrasser@sbcglobal.net>
2021-11-12 13:47:22 -0800

    get single album backend route works

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -2,10 +2,24 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { check, validationResult } = require('express-validator');
 
-const { Album } = require('../../db/models');
+const { Album, Image } = require('../../db/models');
 
 const router = express.Router();
 
+// Get single album
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const albumId = req.params.id;
+    const album = await Album.findByPk(albumId, {
+        include: {
+            model: Image
+        }
+    });
 
+    if (album) {
+        res.json(album)
+    } else {
+        res.send('Album not found')
+    }
+}));
 
 module.exports = router;
