5e0a94d432bffd9dcb10abd666fb53c4d5cd9466
Rob Strasser <roestrasser@sbcglobal.net>
2022-01-21 14:53:24 -0800

    backend post comments route

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -17,7 +17,15 @@ const editValidation = [
     check('content')
         .exists({ checkFalsy: true })
         .withMessage('Please provide Content for your image.')
-]
+];
+
+const addCommentValidation = [
+    check('comment')
+        .exists({ checkFalsy: true })
+        .withMessage('Comment cannot be blank')
+        .isLength({ max: 255 })
+        .withMessage('Comment needs to be less than 255 characters')
+];
 
 // Get all images
 router.get('/', asyncHandler(async (req, res) => {
@@ -62,9 +70,24 @@ router.put('/:id(\\d+)', editValidation, asyncHandler(async (req, res, next) => 
     }
 }));
 
-// All Comments
-// router.get('/:id(\\d+)/comments', asyncHandler(async (req, res, next) => {
+// Add Comment
+router.post('/:id(\\d+)/comments', addCommentValidation, asyncHandler(async (req, res, next) => {
+    const imageId = req.params.id;
+    const { userId, comment } = req.body;
+    const addCommentErrors = validationResult(req);
 
-// }));
+    if (addCommentErrors.isEmpty()) {
+        const newComment = await Comment.build({
+            imageId,
+            userId,
+            comment
+        });
+        await newComment.save();
+        res.json(newComment);
+    } else {
+        let errors = addCommentErrors.array().map((error) => error.msg);
+        res.json({ errors });
+    }
+}));
 
 module.exports = router;
